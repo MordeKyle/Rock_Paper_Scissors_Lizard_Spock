54ad21733c4d0c9bcc241b9b7c22a56e26077a43
Kyle McBride <Kyle@MordeKyle.com>
2014-03-19 02:08:09 -0500

    Version: 1.0 Genesis Robin
    03/18/2014 @10:57
    •Adding everything to form
    radio buttons
    resultLabel
    computerChoiceLabel
    playButton
    •Added all of the variables
    •Added function generateRandom
    •Added function compareResults
    •Icon added to form
    •Added function rockSelected
    •Added function paperSelected

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rockButton = new System.Windows.Forms.RadioButton();
+            this.paperButton = new System.Windows.Forms.RadioButton();
+            this.scissorsButton = new System.Windows.Forms.RadioButton();
+            this.lizardButton = new System.Windows.Forms.RadioButton();
+            this.spockButton = new System.Windows.Forms.RadioButton();
+            this.playButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.computerChoiceLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rockButton);
+            this.groupBox1.Controls.Add(this.paperButton);
+            this.groupBox1.Controls.Add(this.scissorsButton);
+            this.groupBox1.Controls.Add(this.lizardButton);
+            this.groupBox1.Controls.Add(this.spockButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(102, 139);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select One:";
+            // 
+            // rockButton
+            // 
+            this.rockButton.AutoSize = true;
+            this.rockButton.Location = new System.Drawing.Point(6, 19);
+            this.rockButton.Name = "rockButton";
+            this.rockButton.Size = new System.Drawing.Size(51, 17);
+            this.rockButton.TabIndex = 1;
+            this.rockButton.Text = "Rock";
+            this.rockButton.UseVisualStyleBackColor = true;
+            // 
+            // paperButton
+            // 
+            this.paperButton.AutoSize = true;
+            this.paperButton.Location = new System.Drawing.Point(6, 42);
+            this.paperButton.Name = "paperButton";
+            this.paperButton.Size = new System.Drawing.Size(53, 17);
+            this.paperButton.TabIndex = 2;
+            this.paperButton.Text = "Paper";
+            this.paperButton.UseVisualStyleBackColor = true;
+            // 
+            // scissorsButton
+            // 
+            this.scissorsButton.AutoSize = true;
+            this.scissorsButton.Location = new System.Drawing.Point(6, 65);
+            this.scissorsButton.Name = "scissorsButton";
+            this.scissorsButton.Size = new System.Drawing.Size(64, 17);
+            this.scissorsButton.TabIndex = 3;
+            this.scissorsButton.Text = "Scissors";
+            this.scissorsButton.UseVisualStyleBackColor = true;
+            // 
+            // lizardButton
+            // 
+            this.lizardButton.AutoSize = true;
+            this.lizardButton.Location = new System.Drawing.Point(6, 88);
+            this.lizardButton.Name = "lizardButton";
+            this.lizardButton.Size = new System.Drawing.Size(53, 17);
+            this.lizardButton.TabIndex = 4;
+            this.lizardButton.Text = "Lizard";
+            this.lizardButton.UseVisualStyleBackColor = true;
+            // 
+            // spockButton
+            // 
+            this.spockButton.AutoSize = true;
+            this.spockButton.Location = new System.Drawing.Point(6, 111);
+            this.spockButton.Name = "spockButton";
+            this.spockButton.Size = new System.Drawing.Size(56, 17);
+            this.spockButton.TabIndex = 5;
+            this.spockButton.Text = "Spock";
+            this.spockButton.UseVisualStyleBackColor = true;
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(180, 177);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Computer\'s Choice:";
+            // 
+            // computerChoiceLabel
+            // 
+            this.computerChoiceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.computerChoiceLabel.Location = new System.Drawing.Point(301, 53);
+            this.computerChoiceLabel.Name = "computerChoiceLabel";
+            this.computerChoiceLabel.Size = new System.Drawing.Size(121, 23);
+            this.computerChoiceLabel.TabIndex = 3;
+            this.computerChoiceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(250, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Results:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultLabel.Location = new System.Drawing.Point(301, 97);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(121, 23);
+            this.resultLabel.TabIndex = 5;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(434, 212);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.computerChoiceLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Rock Paper Scissors Lizard Spock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rockButton;
+        private System.Windows.Forms.RadioButton paperButton;
+        private System.Windows.Forms.RadioButton scissorsButton;
+        private System.Windows.Forms.RadioButton lizardButton;
+        private System.Windows.Forms.RadioButton spockButton;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label computerChoiceLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,115 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*Program: Paper Rock Scissors Lizard Spock
+  Author: Kyle McBride A02609917
+  Date: 03/18/2014
+  Description: Application that lets the user play the game of Rock, Paper, Scissors, Lizard, Spock against the computer.
+                                          ***SEE CHANGELOG IN SOLUTION DIRECTORY***				*/
+
 namespace Rock_Paper_Scissors_Lizard_Spock
 {
     public partial class Form1 : Form
     {
+        private const int superWinConstant = 2;
+        private const int winConstant = 1;
+        private const int drawConstant = 0;
+        private const int loseConstant = -1;
+        private const int superLoseConstant = -2;
+        private const int userInputConstant = 3;
+        private int resultHolder;
+        private int computerChoice;
+        private string rockString = "Rock";
+        private string paperString = "Paper";
+        private string scissorsString = "Scissors";
+        private string lizardString = "Lizard";
+        private string spockString = "Spock";
+        private string winString = "You Win!";
+        private string drawString = "Draw";
+        private string loseString = "You Lose";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void generateRandom()
+        {
+            Random rand = new Random();
+            computerChoice = rand.Next(5) + 1;
+        }
+
+        private void compareResults()
+        {
+            resultHolder = userInputConstant - computerChoice;
+        }
+
+        private void rockSelected()
+        {
+            if(resultHolder == superWinConstant)
+            {
+                computerChoiceLabel.Text = lizardString;
+                resultLabel.Text = winString;
+            }
+            else if(resultHolder == winConstant)
+            {
+                computerChoiceLabel.Text = scissorsString;
+                resultLabel.Text = winString;
+            }
+            else if(resultHolder == drawConstant)
+            {
+                computerChoiceLabel.Text = rockString;
+                resultLabel.Text = drawString;
+            }
+            else if(resultHolder == loseConstant)
+            {
+                computerChoiceLabel.Text = paperString;
+                resultLabel.Text = loseString;
+            }
+            else
+            {
+                computerChoiceLabel.Text = spockString;
+                resultLabel.Text = loseString;
+            }
+        }
+
+        private void paperSelected()
+        {
+            if(resultHolder == superWinConstant)
+            {
+                computerChoiceLabel.Text = spockString;
+                resultLabel.Text = winString;
+            }
+            else if(resultHolder == winConstant)
+            {
+                computerChoiceLabel.Text = rockString;
+                resultLabel.Text = winString;
+            }
+            else if(resultHolder == drawConstant)
+            {
+                computerChoiceLabel.Text = paperString;
+                resultLabel.Text = drawString;
+            }
+            else if(resultHolder == loseConstant)
+            {
+                computerChoiceLabel.Text = scissorsString;
+                resultLabel.Text = loseString;
+            }
+            else
+            {
+                computerChoiceLabel.Text = lizardString;
+                resultLabel.Text = loseString;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
